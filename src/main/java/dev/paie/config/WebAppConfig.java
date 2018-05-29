8e51b5771ee5fc110943a1f52311ae83e6ca392c
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-05-29 15:11:32 +0200

    heroku deploy

--- src/main/java/dev/paie/config/WebAppConfig.java
+++ src/main/java/dev/paie/config/WebAppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("dev.paie.web")
-@Import({ServicesConfig.class, H2Config.class})
+@Import({ServicesConfig.class, HerokuDBConfig.class})
 public class WebAppConfig {
 	@Bean
     public ViewResolver viewResolver() {
